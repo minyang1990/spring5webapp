6c05d56b70b7371ee535b2c0f475fbd58521fe8b
minyang1990 <ocala3526159467@gmail.com>
2018-09-16 03:46:46 -0400

    22

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -9,17 +9,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by jt on 5/18/17.
  */
 @Controller
+//会变成spring bean  spring managw component
 public class BookController {
 
     private BookRepository bookRepository;
 
+    //    由于这里有constroctor 这里会auto wired
     public BookController(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
-
+    //这个会invok 只要url request book进来
     @RequestMapping("/books")
     public String getBooks(Model model){
-
+        //把所有jpa 数据库里的书本拿出来 绑定books srtring 和所有书本
         model.addAttribute("books", bookRepository.findAll());
 
         return "books";
